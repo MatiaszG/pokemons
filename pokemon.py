7810c5f403e76e6bed6be6489a78f39eb61e8abf
Galimatiasz <mateuszgajewski2233553@gmail.com>
2022-07-25 18:22:42 +0200

    Dodanie funkcjonalności wyznaczającej mnożnik

---
+++ pokemon.py
@@ -0,0 +1,12 @@
+from utils import calculate_multiplier
+
+def main():
+    print('Insert attack type: ')
+    attack_type = input()
+    print('Insert pokemon types delimited by single space: ')
+    pokemon_types = list(input().split(' '))
+
+    print(calculate_multiplier(attack_type, pokemon_types))
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,19 @@
+import requests
+
+def calculate_multiplier(attack_type, pokemon_types):
+    response = requests.get("https://pokeapi.co/api/v2/type/"+attack_type)
+    if response.ok:
+        response_data = response.json()
+        multiplier = 1
+        for i in pokemon_types:
+            damage_relations = response_data['damage_relations']
+            if any(d['name'] == i for d in damage_relations['no_damage_to']):
+                multiplier *= 0
+                break
+            elif any(d['name'] == i for d in damage_relations['double_damage_to']):
+                multiplier *= 2
+            elif any(d['name'] == i for d in damage_relations['half_damage_to']):
+                multiplier *= 0.5
+        return(str(multiplier)+'x')
+    else:
+        return('Error')
